ca00eb9573182af87f705de7576b65d09b524c34
Catherine Palermo <cspalermo@hotmail.com>
2020-03-05 17:34:01 -0800

    added lab 08

--- js/app.js
+++ js/app.js
@@ -17,6 +17,42 @@ function askPreference(){
    return document.write('<h3>' + message + '</h3>');
 }
 
+function getItem(){
+    var preference = prompt('Would you like to order chocolate or caramel?');
+    var item;
+    // while true and true // this keeps running / ONE FALSE EJECTS FROM WHILE LOOP
+    while(preference !== 'chocolate' && preference !== 'caramel'){
+      preference = prompt('Would you like chocolate or caramel? PLEASE ANSWER CORRECTLY');
+    }
+    if (preference === 'chocolate') {
+      item = '<img src="images/chocolate.jpeg">';
+    } else if (preference === 'caramel') {
+      item = '<img src="images/caramel.jpeg">';
+    } 
+    return item;
+  }
+
+function howMany(){
+    var count = prompt('Sounds good, how many pieces would you like?');
+    // while true and true it keeps asking for a number
+    while (count === '' || isNaN(count)){
+      var count = prompt('How Many would you like to order?  ENTER A NUMBER!');
+    }
+    return count;
+  }
+
+  function showOrder(){
+    var item = getItem();
+    var total = howMany();
+    result = '';
+  
+    // adds a paragraph with an image in it for each number of times
+    for (var i = 0; i < total; i++){
+      result = result + '<p>' + item + '</p>';
+    }
+  return document.write(result);
+  }
+
 function askGreeting(){
     var today = new Date();
     var hourNow = today.getHours();
